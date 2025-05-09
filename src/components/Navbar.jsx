25d63b7a9ab39d5ce7cdcbb7e87c2341f7e05c70
najmanmna <139351540+najmanmna@users.noreply.github.com>
2025-05-09 19:51:05 +0500

    build error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Menu, X } from "lucide-react";
-import logo from "/PHH_LOGO_NOBG.png";
+
 import DonateButton from "./DonateButton";
 import { Link, useLocation } from "react-router-dom";
 
@@ -18,7 +18,7 @@ const Navbar = ({ variant }) => {
         <div className="flex items-center space-x-3">
           <Link to="/">
             <img
-              src={logo}
+              src="/PHH_LOGO_NOBG.png"
               alt="PHH Logo"
               className="w-16 h-16 object-contain sm:w-[80px] md:w-[100px] md:h-20"
             />
@@ -99,7 +99,10 @@ const Navbar = ({ variant }) => {
       {/* Mobile Menu */}
       {isOpen && (
         <div className="lg:hidden fixed  bg-white/90 backdrop-blur-md inset-x-0 z-50 px-4 pb-4 space-y-4 text-gray-800 font-medium shadow-md">
-          <a href={isDonatePage ||isProjectPage ? "/" : "#home"} className="block ">
+          <a
+            href={isDonatePage || isProjectPage ? "/" : "#home"}
+            className="block "
+          >
             Home
           </a>
           {!isDonatePage && !isProjectPage && (
