3c0b32224fea336622da6b8e28e76c2db6f06afc
najmanmna <139351540+najmanmna@users.noreply.github.com>
2025-05-11 21:04:33 +0500

    build error resolved

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,6 @@
 import About from "../components/About";
 import ContactUs from "../components/ContactUs";
 import CTA from "../components/CTA";
-import ExecutiveBoard from "../components/ExecutiveBoard";
 import Hero from "../components/Hero";
 import ProjectHighlights from "../components/HighlightedProjects";
 import MessagesCarousel from "../components/MessagesCarousel";
@@ -16,7 +15,6 @@ function Homepage() {
       <About />
       <Projects />
       <ProjectHighlights />
-      {/* <ExecutiveBoard /> */}
       <CTA />
       <MessagesCarousel />
       <ContactUs />
